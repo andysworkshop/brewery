fb319bd48020dbd531b534089c35c99130057114
Andy Brown <andy@andybrown.me.uk>
2018-02-11 17:37:07 +0000

    update comments

--- firmware/ltc2986-rtd/ProgStrings.h
+++ firmware/ltc2986-rtd/ProgStrings.h
@@ -1,5 +1,5 @@
 /*
- * Andy's Workshop Brewery Relays controller ATMega328p firmware
+ * Andy's Workshop Brewery LTC2986 RTD controller ATMega328p firmware
  * Copyright (c) 2017 Andy Brown. http://www.andybrown.me.uk
  * Please see website for licensing terms.
  */
